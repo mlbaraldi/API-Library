d52d3cefdefc18c2d3a26ae9bdedc69facbcc7a6
Marcelo Baraldi <48130176+mlbaraldi@users.noreply.github.com>
2022-05-10 08:03:33 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb+srv://mlbaraldi:Stellinh4@Testes-Mongo.nfelt.mongodb.net/AluraAbril")
+    await mongoose.connect("mongodb+srv://")
   }
 
 let db = mongoose.connection
